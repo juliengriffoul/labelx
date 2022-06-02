12926d3db233092373db3d7c12d3e20fe49f6dcc
Julien Griffoul <julien.griffoul@viacesi.fr>
2022-06-02 07:41:54 +0200

    :sparkles: Add file module to handle CSV interfacing

---
+++ src/file.py
@@ -0,0 +1,76 @@
+import json
+from csv import reader, writer
+
+
+def parse_config_file(path):
+    with open(path, "r", encoding="utf-8") as file:
+        return json.load(file)
+
+
+def get_length_of_csv_file(path):
+    with open(path, "r", encoding="utf-8") as file:
+        csv_reader = reader(file)
+        return len(list(csv_reader))
+
+
+def append_row_to_csv_file(path, length, fields, row):
+    with open(path, "a", encoding="utf-8") as file:
+        csv_writer = writer(file)
+
+        if length == 0:
+            csv_writer.writerow(fields)
+
+        csv_writer.writerow(row)
+
+
+def get_rows_of_csv_file(path):
+    rows = []
+
+    with open(path, "r", encoding="utf-8") as file:
+        csv_reader = reader(file)
+
+        for row in csv_reader:
+            rows.append(row[:-1])
+
+    return rows
+
+
+def get_first_non_labelled_row(path, labelled_rows):
+    index = -1
+
+    with open(path, encoding="utf-8") as file:
+        csv_reader = reader(file)
+
+        for row in csv_reader:
+            if row not in labelled_rows:
+                current_row = row
+                break
+
+            index += 1
+
+    return index, current_row
+
+
+def get_fields_indexes_of_csv_file(path, selected_fields, last_labelled_row_index):
+    index = 0
+    selected_fields_indexes = []
+    fields = None
+    last_row = None
+
+    with open(path, encoding="utf-8") as file:
+        csv_reader = reader(file)
+
+        for row in csv_reader:
+            if not fields:
+                fields = row
+                for field in selected_fields:
+                    selected_fields_indexes.append(fields.index(field))
+                continue
+
+            if last_labelled_row_index and index == int(last_labelled_row_index):
+                last_row = row
+                break
+
+            index += 1
+
+    return fields, selected_fields_indexes, index, last_row
