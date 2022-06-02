d23a478debac078226f9ea19f041107541cc72ed
Julien Griffoul <julien.griffoul@viacesi.fr>
2022-06-02 08:50:26 +0200

    :test_tube: Add placeholder test

---
+++ tests/test_files.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class TestFileMethods(unittest.TestCase):
+
+    def test_parse_config_file(self):
+        self.assertEqual(1, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
